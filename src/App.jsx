38cf1e6daaca5a7d5a965d4e36b7fcf4b6a19044
eliott-colin <eliott.colin.pro@gmail.com>
2024-12-17 16:58:18 +0100

    EndOfDay

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <ProfileCard />
         <Skills />
         <Hobbies />
-        <ContactForm />
+        {/* <ContactForm /> */}
       </main>
     </div>
   );
--- src/components/hobbies.jsx
+++ src/components/hobbies.jsx
@@ -1,28 +1,65 @@
   import React from 'react';
   import styled from 'styled-components';
 
+  
+  const HobbyContainer = styled.div `
+  width:500px;
+  height:500px;
+  display:grid;
+  grid-template-columns: repeat(2, 1fr); /* 2 colonnes de taille égale */
+  grid-template-rows: repeat(2, 1fr);
+  `;
+  
+  
   const HobbyCard = styled.div`
     width:220px;
     height:220px;
+    display:flex;
+    align-items:center;
+    flex-direction:column;
+    border-radius:30px;
+    overflow:hidden
   `;
+  const HobbyImage = styled.img`;
+    height:100%;
+  
+  `;
+  const HobbyH3 = styled.h3`
+  position:relative;
+  width:80%;
+  bottom:0px;
+  border-radius:30px;
+  `
+
+  const HobbyH3Container = styled.span`
+  position:relative;
+  width:80%;
+  bottom:0px;
+  border-radius:30px;
+  transform: translateY(-100%);
+  background-color: #FFFFFF;
+  opacity:26%;
+  `
 
   const hobby = [
-    { title: 'Jeux Vidéo', image: '../src/assets/cat.gif' },
+    { title: 'Jeux Vidéo', image: '../src/assets/turntable.gif' },
     { title: 'Sports', image: '../src/assets/snowboard.gif' },
     { title: 'Développement', image: '../src/assets/pacman.gif' },
-    { title: 'Musique', image: '../src/assets/turntable.gif' },
+    { title: 'Musique', image: '../src/assets/cat.gif' },
   ];
 
   const hobbies = () => {
     return (
-      <div className="hobbies">
+      <HobbyContainer>
         {hobby.map((hobby, index) => (
           <HobbyCard>
-            <img src={hobby.image} alt={hobby.title} />
-            <h3>{hobby.title}</h3>
+            <HobbyImage src={hobby.image} alt={hobby.title} />
+            <HobbyH3Container>
+              <HobbyH3>{hobby.title}</HobbyH3>
+            </HobbyH3Container>
           </HobbyCard>
         ))}
-      </div>
+      </HobbyContainer>
     );
   };
 
