4eafe41905e332b15bb482534dcb6e00292f6249
eliott-colin <eliott.colin.pro@gmail.com>
2024-12-17 15:37:45 +0100

    Uptodate

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const NavHeader = styled.nav`
+  color: red;
+  font-size: 20px;
+`;
+const UlHeader = styled.li`
+  display:flex;
+  gap:10px;
+`;
+const LiHeader = styled.li`
+  display:flex;
+  background-color:#1B1919;
+  border-radius:12px;
+  gap:10px;
+  padding:5px;
+`;
+
 
 const header = () => {
   return (
-    <nav>
-      <ul>
-        <li><a href="#homepage">Homepage</a></li>
-        <li><a href="#projects">Projets</a></li>
-        <li><a href="#skills">Compétences</a></li>
-      </ul>
-    </nav>
+    <NavHeader>
+      <UlHeader>
+        <LiHeader><a href="#homepage">Homepage</a></LiHeader>
+        <LiHeader><a href="#projects">Projets</a></LiHeader>
+        <LiHeader><a href="#skills">Compétences</a></LiHeader>
+      </UlHeader>
+    </NavHeader>
   );
 };
 
 export default header;
+
+
--- src/components/hobbies.jsx
+++ src/components/hobbies.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
+  import React from 'react';
+  import styled from 'styled-components';
 
-const hobby = [
-  { title: 'Jeux Vidéo', image: './assets/cat.gif' },
-  { title: 'Sports', image: './assets/snowboard.png' },
-  { title: 'Développement', image: 'assets/cat.png' },
-  { title: 'Musique', image: 'assets/turntable.png' },
-];
+  const HobbyCard = styled.div`
+    width:220px;
+    height:220px;
+  `;
 
-const hobbies = () => {
-  return (
-    <div className="hobbies">
-      {hobby.map((hobby, index) => (
-        <div key={index} className="hobby-card">
-          <img src={hobby.image} alt={hobby.title} />
-          <h3>{hobby.title}</h3>
-        </div>
-      ))}
-    </div>
-  );
-};
+  const hobby = [
+    { title: 'Jeux Vidéo', image: '../src/assets/cat.gif' },
+    { title: 'Sports', image: '../src/assets/snowboard.gif' },
+    { title: 'Développement', image: '../src/assets/pacman.gif' },
+    { title: 'Musique', image: '../src/assets/turntable.gif' },
+  ];
 
-export default hobbies;
+  const hobbies = () => {
+    return (
+      <div className="hobbies">
+        {hobby.map((hobby, index) => (
+          <HobbyCard>
+            <img src={hobby.image} alt={hobby.title} />
+            <h3>{hobby.title}</h3>
+          </HobbyCard>
+        ))}
+      </div>
+    );
+  };
+
+  export default hobbies;
