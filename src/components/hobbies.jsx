9600928cc8e354df3c974f812280d1859fe171dd
eliott-colin <eliott.colin.pro@gmail.com>
2024-12-17 10:40:27 +0100

    Working branch

--- src/components/hobbies.jsx
+++ src/components/hobbies.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
 const hobby = [
-  { title: 'Jeux Vidéo', image: 'assets/pacman.png' },
-  { title: 'Sports', image: 'assets/snowboard.png' },
-  { title: 'Développement', image: 'assets/coding-cat.png' },
+  { title: 'Jeux Vidéo', image: './assets/cat.gif' },
+  { title: 'Sports', image: './assets/snowboard.png' },
+  { title: 'Développement', image: 'assets/cat.png' },
   { title: 'Musique', image: 'assets/turntable.png' },
 ];
 
 const hobbies = () => {
   return (
     <div className="hobbies">
-      {/* {hobbies.map((hobby, index) => (
+      {hobby.map((hobby, index) => (
         <div key={index} className="hobby-card">
           <img src={hobby.image} alt={hobby.title} />
           <h3>{hobby.title}</h3>
         </div>
-      ))} */}
+      ))}
     </div>
   );
 };
