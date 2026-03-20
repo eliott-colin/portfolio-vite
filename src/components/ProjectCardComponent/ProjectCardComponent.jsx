dc6e2a41aeb30c95aadaf4ac9eb4f187bb7eba2f
Eliott COLIN <eliott.colin@andromede.local>
2026-03-20 16:56:50 +0100

    C:/Program Files/Git/feat pages projects add flexend on the card

--- src/components/ProjectCardComponent/ProjectCardComponent.jsx
+++ src/components/ProjectCardComponent/ProjectCardComponent.jsx
@@ -3,12 +3,12 @@ import "./ProjectCardComponent.css";
 
 const ProjectCardComponent = ({ project , index }) => {
   return (
-    <div className="project-card"  style={{ gridColumnStart: (index%2)+1, gridRowStart: index+1, ...(index % 2 === 1 && { justifySelf: "end" }) }} >
+    <div className="project-card"  style={{ gridColumnStart: (index%2)+1, gridRowStart: index+1, ...(index % 2 === 1 && { justifySelf: "end" , alignItems: "end"  }) }} >
       <h3>{project.name}</h3>
       <div className="project-card-wrapper" >
         <img src={project.image} alt={project.name} />
       </div>
-      <p>{project.description}</p>
+      <p>{project.longDescription}</p>
       <a href={project.link} target="_blank" rel="noopener noreferrer">
         Voir le projet
       </a>
